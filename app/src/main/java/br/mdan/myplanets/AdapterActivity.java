4b130983010f23ece722073c190f37d4e291b7f2
MarcinhoFIVE <darlan_lifecat@yahoo.com.br>
2022-06-07 15:39:52 -0300

    External Adapter

---
+++ app/src/main/java/br/mdan/myplanets/AdapterActivity.java
@@ -0,0 +1,46 @@
+package br.mdan.myplanets;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class AdapterActivity extends BaseAdapter {
+
+    int[] photoPlanet = {R.drawable.sun, R.drawable.sun, R.drawable.sun};
+    String[] textPlanet = {"Teste", "Teste", "Teste"};
+
+    private final Context context;
+
+    public AdapterActivity(Context applicationContext) {
+        context = applicationContext;
+    }
+
+    @Override
+    public int getCount() { return photoPlanet.length; }
+
+    @Override
+    public Object getItem(int position) { return null; }
+
+    @Override
+    public long getItemId(int position) { return 0; }
+
+    @SuppressLint("ViewHolder")
+    @Override
+    public View getView(int position, View view, ViewGroup parent) {
+
+        view = LayoutInflater.from(context).inflate(R.layout.model_planets_layout, parent, false);;
+
+        ImageView imagePlanet = view.findViewById(R.id.ivPlanets);
+        TextView stringPlanet = view.findViewById(R.id.tvPlanets);
+
+        imagePlanet.setImageResource(photoPlanet[position]);
+        stringPlanet.setText(textPlanet[position]);
+
+        return view;
+    }
+}
--- app/src/main/java/br/mdan/myplanets/MainActivity.java
+++ app/src/main/java/br/mdan/myplanets/MainActivity.java
@@ -3,12 +3,52 @@ package br.mdan.myplanets;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        AdapterActivity adapter = new AdapterActivity(getApplicationContext());
+        listView = findViewById(R.id.lvPlanets);
+        listView.setAdapter(adapter);
     }
+
+//    public class AquiAdapter extends BaseAdapter {
+//
+//        @Override
+//        public int getCount() {
+//            //return photoPlanet.length;
+//            return 0;
+//        }
+//
+//        @Override
+//        public Object getItem(int position) {
+//            return null;
+//        }
+//
+//        @Override
+//        public long getItemId(int position) {
+//            return 0;
+//        }
+//
+//        @Override
+//        public View getView(int position, View view, ViewGroup parent) {
+//
+//            view = getLayoutInflater().inflate(R.layout.model_planets_layout, null);
+//
+//            ImageView imagePlanet = view.findViewById(R.id.ivPlanets);
+//            TextView namePlanet = view.findViewById(R.id.tvPlanets);
+//
+//           //imagePlanet.setImageResource(photoPlanet[position]);
+//           //namePlanet.setText(textPlanet[position]);
+//
+//            return view;
+//        }
+//    }
 }
